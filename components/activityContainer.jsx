17d9e5639505ce27333317601b382c6b32b7aae5
dityath <dityaathallah107@gmail.com>
2021-10-22 11:18:49 +0700

    back to maintenance

--- components/activityContainer.jsx
+++ components/activityContainer.jsx
@@ -1,19 +1,19 @@
 import styles from '../styles/components/activityContainer.module.scss'
 import Image from 'next/image'
 
-export default function activityContainer(props) {
-    return (
-        <section className={styles.container}>
-            <div className={styles.img}>
-                <Image
-                    src='/coba/sandal.jpg'
-                    alt='Activity photo'
-                    layout='fill'
-                    objectFit='cover'
-                />
-            </div>
-            <h5>Webinar: introducing Design...</h5>
-            <p>25 August 2021</p>
-        </section>
-    )
+export default function activityContainer(title, date, photo) {
+  return (
+    <section className={styles.container}>
+      <div className={styles.img}>
+        <Image
+          src={photo}
+          alt="Activity photo"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h5>{title}</h5>
+      <p>{date}</p>
+    </section>
+  );
 }
--- pages/activity.jsx
+++ pages/activity.jsx
@@ -1,33 +1,48 @@
-import Image from 'next/image'
-import Head from 'next/head';
-import ReactPaginate from 'react-paginate';
+import Head from "next/head";
+import ReactPaginate from "react-paginate";
 
 import Navbar from "../components/navbar";
-import BigTitle from '../components/bigTitle';
+import BigTitle from "../components/bigTitle";
 import Footer from "../components/footer";
-import ActivityContainer from '../components/activityContainer';
+import ActivityContainer from "../components/activityContainer";
 
-import styles from '../styles/pages/activity.module.scss';
-import TopButton from '../components/topButton';
+import styles from "../styles/pages/activity.module.scss";
+import TopButton from "../components/topButton";
+import React, { useState, useEffect } from "react";
+import API from "../api";
+import { BASE_URL } from "../api/const";
 
-function activity() {
-	return (
-		<div className={styles['main-body']}>
-			<Head>
-				<title>Activity</title>
-			</Head>
+function Activity() {
+  const [data, setData] = useState(null);
 
-			<Navbar />
+  useEffect(() => {
+    API.getActivity().then((resp) => setData(resp.data));
+    console.log(data);
+  }, [data]);
 
-			<BigTitle>Our Activity</BigTitle>
+  return (
+    <div className={styles["main-body"]}>
+      <Head>
+        <title>Activity</title>
+      </Head>
 
-			<section className={styles.first}>
-				<ActivityContainer />
-				<ActivityContainer />
-				<ActivityContainer />
-			</section>
+      <Navbar />
 
-			{/* <ReactPaginate
+      <BigTitle>Our Activity</BigTitle>
+
+      {/* <section className={styles.first}>
+        {data?.map((activity) => (
+          <React.Fragment key={activity.id}>
+            <ActivityContainer
+              photo={`${BASE_URL}${activity?.thumbnail}`}
+              title={activity.name}
+              date={activity.updated_at}
+            />
+          </React.Fragment>
+        ))}
+      </section> */}
+
+      {/* <ReactPaginate
                 previousLabel={'previous'}
                 nextLabel={'next'}
                 breakLabel={'...'}
@@ -40,10 +55,10 @@ function activity() {
                 activeClassName={'active'}
             /> */}
 
-			<TopButton />
-			<Footer />
-		</div>
-	)
+      <TopButton />
+      <Footer />
+    </div>
+  );
 }
 
-export default activity;
+export default Activity;
