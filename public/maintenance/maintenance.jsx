0a42be71d790c391b75f05c2b48d91638a00c2d1
dityath <dityaathallah107@gmail.com>
2021-10-22 21:09:07 +0700

    add seo again

--- public/maintenance/maintenance.jsx
+++ public/maintenance/maintenance.jsx
@@ -15,12 +15,13 @@ const Maintenance = () => {
     >
       <Head>
         <title>Maintenance</title>
+        <meta name="og:title" content="Maintenance" key="title" />
         <meta
           name="description"
           content="Sorry, this web is still under maintenance"
           key="description"
         />
-        <meta name="og:title" content="Maintenance" key="title" />
+        <meta name="twitter:title" content="Maintenance" />
         <meta
           name="og:description"
           content="Sorry, this web is still under maintenance"
@@ -28,18 +29,19 @@ const Maintenance = () => {
         />
         <meta
           name="og:image"
-          content="https://res.cloudinary.com/deajg4y52/image/upload/v1634909808/Main_Seo_quhy41.png"
+          content="https://res.cloudinary.com/deajg4y52/image/upload/v1634909880/404_Seo_nwvr9d.png"
         />
         <meta name="og:image:width" content="1200" />
         <meta name="og:image:height" content="600" />
         <meta name="og:type" content="website" />
-
-        <meta name="twitter:title" content="Maintenance" />
         <meta
           name="twitter:description"
           content="Sorry, this web is still under maintenance"
         />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:card"
+          content="https://res.cloudinary.com/deajg4y52/image/upload/v1634909880/404_Seo_nwvr9d.png"
+        />
       </Head>
       <svg
         style={{ width: "80vmin", margin: "6vmin auto" }}
