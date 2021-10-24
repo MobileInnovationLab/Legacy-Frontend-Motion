4916550220ea49520b73d4b07b7db86db5bf1906
dityath <dityaathallah107@gmail.com>
2021-10-24 18:54:53 +0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Maintenance from "../public/maintenance/maintenance";
 
+import ReactGa from "react-ga";
+
+const TRACKING_ID = "G-GHYHBVVWV8";
+ReactGa.initialize(TRACKING_ID);
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-  //return <Maintenance />;
+  //return <Component {...pageProps} />;
+  return <Maintenance />;
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,23 @@ class MyDocument extends Document {
             href="favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="favicon/site.webmanifest"></link>
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
